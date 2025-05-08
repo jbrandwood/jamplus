16f1efa1a43d0cd124265ab77739dea8e5a53700
Josh Jensen <josh@everrush.com>
2025-05-08 09:58:08 -0600

    JamToWorkspace: Make checks for $(JAM_CREATING_WORKSPACE) available

--- bin/scripts/JamToWorkspace.lua
+++ bin/scripts/JamToWorkspace.lua
@@ -328,6 +328,7 @@ function CreateTargetInfoFiles(outPath)
 			ospath.escape('JAMFILE_ROOT=' .. sourceRootPath),
 			ospath.escape('JAMFILE=' .. ospath.join(_getTargetInfoPath(), 'DumpJamTargetInfo.jam')),
 			ospath.escape('TARGETINFO_LOCATE=' .. ospath.add_slash(_getTargetInfoPath())),
+			'JAM_CREATING_WORKSPACE=1',
 			'C.TOOLCHAIN=' .. platform .. '/' .. config,
 			'-d0',
 			'-S'
