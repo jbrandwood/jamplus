3e952064bf0a946de96784086965860b63971912
Josh Jensen <josh@everrush.com>
2025-08-31 13:51:07 -0600

    Enhance builtin Exit to handle other parameter lists

--- src/builtins.c
+++ src/builtins.c
@@ -522,7 +522,13 @@ builtin_exit(
 	LOL	*args,
 	int	*jmp )
 {
-	list_print( lol_get( args, 0 ) );
+	int i;
+	for ( i = 0; i < args->count; ++i ) {
+		if (i > 0) {
+			printf(": ");
+		}
+		list_print( lol_get( args, i ) );
+	}
 	printf( "\n" );
 	exit( EXITBAD ); /* yeech */
 	return L0;
